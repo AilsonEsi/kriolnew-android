8fe361ec75d2ff37c85c5ab4a2f554693b0e074c
Ailson Moreira <ailsonesi96@gmail.com>
2019-06-05 17:50:18 -0100

    Update signup activity

--- app/src/main/java/com/doit/kriolnews_aplicacaodenoticias/account/SignUpActivity.java
+++ app/src/main/java/com/doit/kriolnews_aplicacaodenoticias/account/SignUpActivity.java
@@ -1,15 +1,36 @@
 package com.doit.kriolnews_aplicacaodenoticias.account;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.doit.kriolnews_aplicacaodenoticias.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private EditText email;
+    private EditText password;
+    private Button btn_registrar;
+    private TextView login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        email = findViewById(R.id.email_input);
+        password = findViewById(R.id.password_input);
+        btn_registrar = findViewById(R.id.btn_registrar);
+        login = findViewById(R.id.tv_login);
+    }
+
+
+
+    private void openLoginView(){
+        Intent loginIntent = new Intent(getApplicationContext(),SignUpActivity.class);
+        startActivity(loginIntent);
     }
 }
