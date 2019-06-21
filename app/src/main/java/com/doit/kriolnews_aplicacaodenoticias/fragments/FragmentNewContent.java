63126f67686c1d26e9a0f13baec00f3550990b51
Ailson Moreira <ailsonesi96@gmail.com>
2019-06-21 19:58:51 -0100

    Several Update

--- app/src/main/java/com/doit/kriolnews_aplicacaodenoticias/fragments/FragmentNewContent.java
+++ app/src/main/java/com/doit/kriolnews_aplicacaodenoticias/fragments/FragmentNewContent.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,7 @@ import android.widget.ListView;
 
 import com.doit.kriolnews_aplicacaodenoticias.R;
 import com.doit.kriolnews_aplicacaodenoticias.model.NewsItem;
+import com.doit.kriolnews_aplicacaodenoticias.model.ReadRss;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -31,6 +33,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FragmentNewContent extends Fragment {
+
+    RecyclerView recyclerView;
 
     private ListView listView;
     private FirebaseAuth mAuth;
@@ -52,7 +56,7 @@ public class FragmentNewContent extends Fragment {
         listView = view.findViewById(R.id.lv_rss);
         mAuth = FirebaseAuth.getInstance();
 
-
+        /*
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -61,139 +65,13 @@ public class FragmentNewContent extends Fragment {
                 startActivity(intent);
             }
         });
+        */
 
-        new ProcessInBackGround().execute();
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
+        //Call Read rss asyntask to fetch rss
+        ReadRss readRss = new ReadRss(getActivity(), recyclerView);
+        readRss.execute();
 
         return view;
-    }
-
-    public InputStream getInputStream(URL url){
-
-        try
-        {
-            //openConnection() returns instance that represents a connection to the remote object referred to by the URL
-            //getInputStream() returns a stream that reads from the open connection
-            return url.openConnection().getInputStream();
-        }
-        catch (IOException e)
-        {
-            return null;
-        }
-    }
-
-    private final class ProcessInBackGround extends AsyncTask<Integer,Integer,String> {
-
-        ProgressDialog progressDialog = new ProgressDialog(getActivity());
-        Exception exception = null;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            progressDialog.setMessage("Buscando Noticias ...");
-            progressDialog.show();
-        }
-
-        @Override
-        protected String doInBackground(Integer... params) {
-
-            String[] Urls = {"https://anacao.cv/feed/",};
-            try
-            {
-                URL url = new URL("https://anacao.cv/feed/");
-
-                //creates new instance of PullParserFactory that can be used to create XML pull parsers
-                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-
-                //Specifies whether the parser produced by this factory will provide support
-                //for XML namespaces
-                factory.setNamespaceAware(false);
-
-                //creates a new instance of a XML pull parser using the currently configured
-                //factory features
-                XmlPullParser xpp = factory.newPullParser();
-
-                // We will get the XML from an input stream
-                xpp.setInput(getInputStream(url), "UTF_8");
-
-                /* We will parse the XML content looking for the "<title>" tag which appears inside the "<item>" tag.
-                 * We should take into consideration that the rss feed name is also enclosed in a "<title>" tag.
-                 * Every feed begins with these lines: "<channel><title>Feed_Name</title> etc."
-                 * We should skip the "<title>" tag which is a child of "<channel>" tag,
-                 * and take into consideration only the "<title>" tag which is a child of the "<item>" tag
-                 *
-                 * In order to achieve this, we will make use of a boolean variable called "insideItem".
-                 */
-                boolean insideItem = false;
-
-                // Returns the type of current event: START_TAG, END_TAG, START_DOCUMENT, END_DOCUMENT etc..
-                int eventType = xpp.getEventType(); //loop control variable
-
-                while (eventType != XmlPullParser.END_DOCUMENT)
-                {
-                    NewsItem n = new NewsItem();
-
-                    //if we are at a START_TAG (opening tag)
-                    if (eventType == XmlPullParser.START_TAG)
-                    {
-                        //if the tag is called "item"
-                        if (xpp.getName().equalsIgnoreCase("item"))
-                        {
-                            insideItem = true;
-                        }
-                        //if the tag is called "title"
-                        else if (xpp.getName().equalsIgnoreCase("title"))
-                        {
-                            if (insideItem)
-                            {
-                                // extract the text between <title> and </title>
-                                //n.setTitle(xpp.nextText());
-                                titles.add(xpp.nextText());
-                            }
-                        }
-
-                        //if the tag is called "link"
-                        else if (xpp.getName().equalsIgnoreCase("link"))
-                        {
-                            if (insideItem)
-                            {
-                                // extract the text between <link> and </link>
-                                links.add(xpp.nextText());
-                            }
-                        }
-                    }
-                    //if we are at an END_TAG and the END_TAG is called "item"
-                    else if (eventType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item"))
-                    {
-                        newsItemList.add(n);
-                        insideItem = false;
-                    }
-
-                    eventType = xpp.next(); //move to next element
-                }
-            }
-            catch (MalformedURLException e)
-            {
-                exception = e;
-            }
-            catch (XmlPullParserException e)
-            {
-                exception = e;
-            }
-            catch (IOException e)
-            {
-                exception = e;
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.news_item, titles);
-            listView.setAdapter(adapter);
-            progressDialog.dismiss();
-        }
     }
 }
---
+++ app/src/main/java/com/doit/kriolnews_aplicacaodenoticias/model/ReadRss.java
@@ -0,0 +1,119 @@
+package com.doit.kriolnews_aplicacaodenoticias.model;
+
+import android.os.AsyncTask;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class ReadRss extends AsyncTask<Void,Void,Void> {
+    Context context;
+    String address = "https://expressodasilhas.cv/rss";
+    ProgressDialog progressDialog;
+    ArrayList<NewsItem> feedItems;
+    RecyclerView recyclerView;
+    URL url;
+
+    public ReadRss(Context context, RecyclerView recyclerView) {
+        this.recyclerView = recyclerView;
+        this.context = context;
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Loading...");
+    }
+
+    //before fetching of rss statrs show progress to user
+    @Override
+    protected void onPreExecute() {
+        progressDialog.show();
+        super.onPreExecute();
+    }
+
+
+    //This method will execute in background so in this method download rss feeds
+    @Override
+    protected Void doInBackground(Void... params) {
+        //call process xml method to process document we downloaded from getData() method
+        ProcessXml(Getdata());
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        progressDialog.dismiss();
+        NewsAdapter adapter = new NewsAdapter(context, feedItems);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.addItemDecoration(new VerticalSpace(20));
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    // In this method we will process Rss feed  document we downloaded to parse useful information from it
+    private void ProcessXml(Document data) {
+        if (data != null) {
+            feedItems = new ArrayList<>();
+            Element root = data.getDocumentElement();
+            Node channel = root.getChildNodes().item(1);
+            NodeList items = channel.getChildNodes();
+            for (int i = 0; i < items.getLength(); i++) {
+                Node cureentchild = items.item(i);
+                if (cureentchild.getNodeName().equalsIgnoreCase("item")) {
+                    NewsItem item = new NewsItem();
+                    NodeList itemchilds = cureentchild.getChildNodes();
+                    for (int j = 0; j < itemchilds.getLength(); j++) {
+                        Node cureent = itemchilds.item(j);
+                        if (cureent.getNodeName().equalsIgnoreCase("title")) {
+                            item.setTitle(cureent.getTextContent());
+                        } else if (cureent.getNodeName().equalsIgnoreCase("description")) {
+                            item.setDescription(cureent.getTextContent());
+                        } else if (cureent.getNodeName().equalsIgnoreCase("pubDate")) {
+                            item.setPubDate(cureent.getTextContent());
+                        } else if (cureent.getNodeName().equalsIgnoreCase("link")) {
+                            item.setLink(cureent.getTextContent());
+                        } else if (cureent.getNodeName().equalsIgnoreCase("enclosure")) {
+                            //this will return us thumbnail url
+                            String url = cureent.getAttributes().item(0).getTextContent();
+                            item.setThumbnailUrl(url);
+                        }
+                    }
+                    feedItems.add(item);
+
+
+                }
+            }
+        }
+    }
+
+    //This method will download rss feed document from specified url
+    public Document Getdata() {
+        try {
+            url = new URL(address);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            InputStream inputStream = connection.getInputStream();
+            DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = builderFactory.newDocumentBuilder();
+            Document xmlDoc = builder.parse(inputStream);
+            return xmlDoc;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
